113b06142c2515a7960c29ec3e5718bb49545f2b
Adityagarg009 <122074991+Adityagarg009@users.noreply.github.com>
2023-12-16 12:35:07 +0530

    speech.py

--- speech.py
+++ speech.py
@@ -1,9 +1,11 @@
 import speech_recognition as sr
 import webbrowser as wb
-
+import pyttsx3 
 chrome_path = "content\\Google\\Chrome\\Application\\chrome.exe %s"
 r = sr.Recognizer()
-
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
 with sr.Microphone() as source:
     print("Say something")
     audio = r.listen(source, timeout=3)
@@ -12,6 +14,9 @@ with sr.Microphone() as source:
 text = r.recognize_google(audio, show_all=False)
 if text:
     print('Sarah thinks you said ---> ' + text)
+    #speaking mode
+    engine.say("opening browser saver")
+    engine.runAndWait()
     search_url = 'https://www.google.com/search?q=' + text
     print("Opening the browser...")
     wb.open(search_url, new=2)  
